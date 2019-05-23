3b0885ff9a2e164bf231230fb622be71ec9334c8
fsampedro <fsampedro@indra.es>
2019-05-23 08:38:11 +0200

    bumping version to 1.3

---
+++ src/main/java/clinic/programming/training/Application.java
@@ -0,0 +1,40 @@
+package clinic.programming.training;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.commons.lang3.StringUtils;
+
+public class Application {
+	
+	public int countWords(String words) {
+		String[] separateWords = StringUtils.split(words,' ');
+		if ( separateWords == null) {
+			return 0;
+		}else {
+			return separateWords.length;
+		}
+	}	
+	
+	
+	public void greet(){
+		List<String> greetings = new ArrayList<>();
+		greetings.add("Hello");
+		
+		for ( String greeting : greetings){
+			System.out.println("Greeting: " + greeting);
+		}
+	}
+    
+    public Application() {
+        System.out.println ("Inside Application");
+    }
+
+    // method main(): ALWAYS the APPLICATION entry point
+    public static void main (String[] args) {
+    	System.out.println ("Starting Application");
+		Application app = new Application();
+		app.greet();
+		int count = app.countWords( "I have four words");
+		System.out.println( "Word Count: "+ count);
+    }
+}
